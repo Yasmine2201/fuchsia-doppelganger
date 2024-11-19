d33555b7fee1687f9a687bec86199525a8e85b8e
Charlotte Bigaré <81164857+KajikaSan@users.noreply.github.com>
2024-11-19 15:33:56 +0100

    Update test_safe_calculator.py

--- python/tests/test_safe_calculator.py
+++ python/tests/test_safe_calculator.py
@@ -1,7 +1,20 @@
 import pytest
+from safe_calculator import SafeCalculator
+
+class Authorizer:
+    def authorize(self):
+        return True
 
 
 def test_divide_should_not_raise_any_error_when_authorized():
+    
     # TODO: write a test that fails due to the bug in
-    # SafeCalculator.add
-    pass
+    safe_calculator = SafeCalculator(Authorizer())
+
+    actual = safe_calculator.add(1, 2)
+    expected = 3
+    
+    assert actual == expected, f'The result should be {expected} but was {actual}'
+        
+
+
